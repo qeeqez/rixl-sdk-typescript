bc6afb345c88fba0bd4cef450e262f34c20e2133
Yusuf Akinleye <ayaaakinleye@gmail.com>
2026-03-25 18:34:48 +0100

    Replace TypeScript SDK with Kiota

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,5 +1,5 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './runtime';
-export * from './apis/index';
-export * from './models/index';
+export * from "./rixlClient.js";
+export * from "./feeds/index.js";
+export * from "./images/index.js";
+export * from "./videos/index.js";
+export * from "./models/index.js";
